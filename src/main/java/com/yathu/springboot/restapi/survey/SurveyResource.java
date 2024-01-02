0a66e209eba77078eecc877db1b28f805f78c9b8
yathu1 <yathu2198@gmail.com>
2024-01-02 22:00:35 +0530

    Get/Surveys

--- src/main/java/com/yathu/springboot/restapi/survey/SurveyResource.java
+++ src/main/java/com/yathu/springboot/restapi/survey/SurveyResource.java
@@ -1,9 +1,21 @@
 package com.yathu.springboot.restapi.survey;
 
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
 
 @RestController
 public class SurveyResource {
 
+    private SurveyService surveyService;
 
+    public SurveyResource(SurveyService surveyService){
+        this.surveyService =surveyService;
+    }
+
+    @RequestMapping("/surveys")
+    public List<Survey> retrieveAllSurveys(){
+        return surveyService.retrieveAllSurveys();
+    }
 }
